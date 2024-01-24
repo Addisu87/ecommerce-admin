f4e06d975be874dc1d69452d18d23259cddcb1a2
Addisu <addisuhaile87@gmail.com>
2024-01-24 10:03:16 +0300

    Applay setting functionality to add new store

---
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -0,0 +1,41 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import prismadb from "@/lib/prismadb";
+
+import SettingsForm from "./components/settings-form";
+
+interface SettingsPageProps {
+  params: {
+    storeId: string;
+  };
+}
+
+const SettingsPage: React.FC<SettingsPageProps> = async ({ params }) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
+
+  if (!store) {
+    redirect("/");
+  }
+
+  return (
+    <div className="">
+      <div className="flex-col">
+        <SettingsForm initialData={store} />
+      </div>
+    </div>
+  );
+};
+
+export default SettingsPage;
