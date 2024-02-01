5bd9077a9a7642a67c841816079a58a8046a8124
Addisu <addisuhaile87@gmail.com>
2024-02-01 18:01:40 +0300

    Update skeleton loding for dashboard page

--- app/(dashboard)/[storeId]/(routes)/loading.tsx
+++ app/(dashboard)/[storeId]/(routes)/loading.tsx
@@ -5,6 +5,14 @@ import { lusitana } from "@/components/ui/fonts";
 
 const shimmer =
   "before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent";
+export const HeadingSkeleton: React.FC = () => {
+  return (
+    <div className="mb-2 space-y-2 w-36">
+      <div className="h-8 overflow-hidden rounded-md bg-gray-100 animate-shimmer_2s_infinite" />
+      <div className="h-4 overflow-hidden rounded-md bg-gray-100 animate-shimmer_2s_infinite" />
+    </div>
+  );
+};
 
 const CardSkeleton: React.FC = () => {
   return (
@@ -29,9 +37,6 @@ const CardSkeleton: React.FC = () => {
 const OverviewSkeleton: React.FC = () => {
   return (
     <div className={`${shimmer} relative w-11/12 mx-auto overflow-hidden`}>
-      <div className="mb-4 h-8 w-36 rounded-md bg-gray-100">
-        <Skeleton className="w-full h-full bg-white" />
-      </div>
       <Card className="rounded-xl bg-gray-100 p-2 shadow-sm">
         <CardHeader>
           <CardTitle className="ml-2 text-xl font-semibold">
@@ -39,7 +44,7 @@ const OverviewSkeleton: React.FC = () => {
           </CardTitle>
         </CardHeader>
         <CardContent className="pl-2">
-          <div className="mt-0 grid h-[410px] grid-cols-12 items-end gap-2 rounded-md bg-white p-4 sm:grid-cols-13 md:gap-4">
+          <div className="mt-0 grid h-[300px] grid-cols-12 items-end gap-2 rounded-md bg-white p-4 sm:grid-cols-13 md:gap-4">
             {Array.from({ length: 12 }).map((_, index) => (
               <Skeleton key={index} className="w-full h-8 bg-white" />
             ))}
@@ -54,6 +59,7 @@ const Loading: React.FC = () => {
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-4">
+        <HeadingSkeleton />
         <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
           {Array.from({ length: 3 }).map((_, index) => (
             <CardSkeleton key={index} />
